e4e540d7b248482d1d9b355f25530e292e4c84d8
suads <timirkosarev@gmail.com>
2026-07-05 22:20:30 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAnrXmovXmBIGocOsbJxSIQ_7dwlHpccdU",
+  authDomain: "edit-web.firebaseapp.com",
+  databaseURL: "https://edit-web-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "edit-web",
+  storageBucket: "edit-web.firebasestorage.app",
+  messagingSenderId: "235371037065",
+  appId: "1:235371037065:web:a44cc11617acb19276e158",
+  measurementId: "G-7VJJSECTXX"
+};
