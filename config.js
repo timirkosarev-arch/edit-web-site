e5a469f118bec9ccb325b8c56df2ce1f95de6398
suads <timirkosarev@gmail.com>
2026-07-15 01:33:05 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,5 @@
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyAxo4VkVVd8PqGJdM5ncIC-W8mQvU0z9uI",
   authDomain: "grt4r-7758d.firebaseapp.com",
   projectId: "grt4r-7758d",
@@ -11,8 +8,3 @@ const firebaseConfig = {
   appId: "1:1044260841178:web:271150cdbb31a90794e7e3",
   measurementId: "G-5GY6WG5PLT"
 };
-
-// Инициализация
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
