82a362a225158657a27f01284c8ae0153c786dee
suads <timirkosarev@gmail.com>
2026-07-20 22:06:54 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,13 @@
-
+// config.js
 export const firebaseConfig = {
-  apiKey: "AIzaSyAxo4VkVVd8PqGJdM5ncIC-W8mQvU0z9uI",
-  authDomain: "grt4r-7758d.firebaseapp.com",
-  projectId: "grt4r-7758d",
-  storageBucket: "grt4r-7758d.firebasestorage.app",
-  messagingSenderId: "1044260841178",
-  appId: "1:1044260841178:web:271150cdbb31a90794e7e3",
-  measurementId: "G-5GY6WG5PLT"
+    apiKey: "AIzaSyDMdx6qrKcNoiSiq7ZO1SvH3y8sPcDS9Bg",
+  authDomain: "p2pkz-47519.firebaseapp.com",
+  projectId: "p2pkz-47519",
+  storageBucket: "p2pkz-47519.firebasestorage.app",
+  messagingSenderId: "403784004236",
+  appId: "1:403784004236:web:60850c184b6cdff90841a9",
+  measurementId: "G-PJNYW1Y05H"
 };
+
+// Твой ключ ImgBB
+export const imgbbApiKey = "91baa56297963d72b88d39e4aee75835";
